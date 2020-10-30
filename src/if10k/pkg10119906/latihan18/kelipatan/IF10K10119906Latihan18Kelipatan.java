f3016df8866e45f0a8144140dd2036408a8fa8f6
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-30 21:07:10 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan18/kelipatan/IF10K10119906Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan18.kelipatan;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama     : Rizqy Ananda Rusmana
+ * Kelas    : IF-10K
+ * NIM      : 10119906
+ * Deskripsi Program : Program perulangan menampilkan kelipatan 3.5 secara ascending
+ * 
+ */
+public class IF10K10119906Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i);      
+        }
+    }
+}
